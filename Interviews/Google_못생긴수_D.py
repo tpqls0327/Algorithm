060a0c572f1bdb54b23f790da6365b3b4e791b8f
ponopono0322 <ponopono0322@gmail.com>
2022-06-23 15:35:14 +0900

    Create Google_못생긴수_D.py

---
+++ Interviews/Google_못생긴수_D.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [0 for _ in range(1001)]
+dp[1] = 1
+
+for i in range(2, 1001):
+    if not i % 2 or not i % 3 or not i % 5:
+        dp[i] = 1
+        
+ans = [i for i in range(1001) if dp[i]]
+print(ans[n-1])

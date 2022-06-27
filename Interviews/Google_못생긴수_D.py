419827052ec9f5373adaf7cd84f25726003f0548
ponopono0322 <ponopono0322@gmail.com>
2022-06-27 11:10:07 +0900

    Update Google_못생긴수_D.py

--- Interviews/Google_못생긴수_D.py
+++ Interviews/Google_못생긴수_D.py
@@ -3,8 +3,9 @@ dp = [0 for _ in range(1001)]
 dp[1] = 1
 
 for i in range(2, 1001):
-    if not i % 2 or not i % 3 or not i % 5:
+    if (not i % 2 or not i % 3 or not i % 5) and i % 7:
         dp[i] = 1
         
 ans = [i for i in range(1001) if dp[i]]
+
 print(ans[n-1])

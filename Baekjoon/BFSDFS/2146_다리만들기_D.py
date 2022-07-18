f3c9fe1273d289c49c00b6d3b1114195c9941433
ponopono0322 <ponopono0322@gmail.com>
2022-07-19 01:20:10 +0900

    Update 2146_다리만들기_D.py

--- Baekjoon/BFSDFS/2146_다리만들기_D.py
+++ Baekjoon/BFSDFS/2146_다리만들기_D.py
@@ -8,6 +8,7 @@ board = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0]*n for _ in range(n)]
 
 
+# 섬 번호 추가해주는 함수(1~n)
 def countries(i, j):
     q = deque([[i, j]])
 
@@ -23,6 +24,7 @@ def countries(i, j):
                 q.append([nx, ny])
 
 
+# 가까운 거리의 섬 찾는 함수
 def bfs(q, cnt):
     while q:
         x, y, z = q.popleft()
@@ -31,14 +33,14 @@ def bfs(q, cnt):
             nx, ny = x + dx[k], y + dy[k]
 
             if 0 <= nx < n and 0 <= ny < n:
-                if not ways[nx][ny] and not board[nx][ny]:
+                if not ways[nx][ny] and not board[nx][ny]:  # 아직 방문하지 않은 바다
                     ways[nx][ny] = 1
-                    q.append([nx, ny, z+1])
+                    q.append([nx, ny, z+1])     # 최소거리가 보장되므로 +1씩
                 elif visited[nx][ny] and board[nx][ny] and board[nx][ny] != cnt:
-                    return z
+                    return z        # 다른 섬을 만났다면 즉시 중지 및 반환
 
 
-c_cnt = 1
+c_cnt = 1   # 섬 번호
 for i in range(n):
     for j in range(n):
         if not visited[i][j] and board[i][j]:
@@ -47,10 +49,10 @@ for i in range(n):
             countries(i, j)
             c_cnt += 1
 
-ans = int(1e9)
+ans = int(1e9)  # 가장 가까운 거리
 for c in range(1, c_cnt):
     que = deque()
-    ways = [[0] * n for _ in range(n)]
+    ways = [[0] * n for _ in range(n)]  # 0 순회할 visited 배열
 
     for i in range(n):
         for j in range(n):

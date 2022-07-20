dd0e45b909ba21af46f586870b0b69ca620fba1a
ponopono0322 <ponopono0322@gmail.com>
2022-07-20 23:48:24 +0900

    Create 1780_종이의개수_D.py

---
+++ Baekjoon/Recursive/1780_종이의개수_D.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10000)
+# input = sys.stdin.readline
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+ans = [0]*3     # [0, 1, -1]
+
+
+def ze(x, y, size):
+    if size < 1:
+        return
+    start = board[x][y]
+
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if board[i][j] != start:
+                tmp = size // 3
+                for a in range(x, x + size, tmp):
+                    for b in range(y, y + size, tmp):
+                        ze(a, b, tmp)
+                return
+
+    ans[start] += 1
+
+
+ze(0, 0, n)
+
+for z in range(-1, 2, 1):
+    print(ans[z])

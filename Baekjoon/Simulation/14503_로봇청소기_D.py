8c2614857ff8c7447f125a531eaaddc5d540078f
ponopono0322 <ponopono0322@gmail.com>
2022-08-12 16:01:15 +0900

    Update 14503_로봇청소기_D.py

--- Baekjoon/Simulation/14503_로봇청소기_D.py
+++ Baekjoon/Simulation/14503_로봇청소기_D.py
@@ -9,57 +9,38 @@ visited = [[0]*m for _ in range(n)]
 def direction(pos):
     dx, dy = [], []
     if pos == 0:
-        dx = [0, 1, 0, -1]
-        dy = [-1, 0, 1, 0]
+        dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
     elif pos == 1:
-        dx = [-1, 0, 1, 0]
-        dy = [0, -1, 0, 1]
+        dx, dy = [-1, 0, 1, 0], [0, -1, 0, 1]
     elif pos == 2:
-        dx = [0, -1, 0, 1]
-        dy = [1, 0, -1, 0]
+        dx, dy = [0, -1, 0, 1], [1, 0, -1, 0]
     elif pos == 3:
-        dx = [1, 0, -1, 0]
-        dy = [0, 1, 0, -1]
-
+        dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
     return dx, dy
 
 
-q = deque()
-q.append([r, c, d])
-visited[r][c] = 1
-is_continue = True
+q = deque([[r, c, d]])
+visited[r][c], ans = 1, 1
 while q:
-    # for v in visited:
-    #     print(v)
-    # print()
     x, y, z = q.popleft()
     tx, ty = direction(z)
     for i in range(4):
-        nx = x + tx[i]
-        ny = y + ty[i]
+        nx, ny = x + tx[i], y + ty[i]
 
         if 0 > nx or nx >= n or 0 > ny or ny >= m:
-            if i == 0:
-                z = z - 1 if z > 1 else 3
+            if not i:
+                z = z - 1 if z >= 1 else 3
                 q.append([x, y, z])
             break
 
-        if board[nx][ny] == 0 and not visited[nx][ny]:
+        if not board[nx][ny] and not visited[nx][ny]:
             visited[nx][ny] = 1
+            ans += 1
             for _ in range(i+1):
-                if z - 1 < 0:
-                    z = 3
-                else:
-                    z -= 1
+                z = z - 1 if z >= 1 else 3
             q.append([nx, ny, z])
             break
-        elif i == 3:
-            if board[x+tx[1]][y+ty[1]] == 0:
-                q.append([x+tx[1], y+ty[1], z])
-
-
-ans = 0
-for v in visited:
-    ans += sum(v)
+        elif i == 3 and not board[x+tx[1]][y+ty[1]]:
+            q.append([x+tx[1], y+ty[1], z])
 
 print(ans)

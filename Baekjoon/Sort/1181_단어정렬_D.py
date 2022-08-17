34cf133aed7f414a3fcf87c22fd8ef205e35d325
ponopono0322 <ponopono0322@gmail.com>
2022-08-17 22:16:51 +0900

    Create 1181_단어정렬_D.py

---
+++ Baekjoon/Sort/1181_단어정렬_D.py
@@ -0,0 +1,11 @@
+import sys
+n = int(input())
+dic = {i: set() for i in range(1, 51)}
+
+for _ in range(n):
+    word = sys.stdin.readline().rstrip()
+    dic[len(word)].add(word)
+
+for i in range(1, 51):
+    if dic[i]:
+        print('\n'.join(sorted(list(dic[i]))))

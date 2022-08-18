2cfdbc56875f29e86192785214f62f9177e2a13c
ponopono0322 <ponopono0322@gmail.com>
2022-08-18 23:09:33 +0900

    Create 11651_좌표정렬하기2_D.py

--- Baekjoon/Sort/11651_좌표정렬하기2_D.py
+++ Baekjoon/Sort/11651_좌표정렬하기2_D.py
@@ -1,4 +1,4 @@
 import sys
 arr = [list(map(int, sys.stdin.readline().split())) for _ in range(int(input()))]
-for i, j in sorted(arr, key=lambda x: (x[0], x[1])):
+for i, j in sorted(arr, key=lambda x: (x[1], x[0])):
     print(i, j)

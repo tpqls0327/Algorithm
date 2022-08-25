e404020eeeb945013736302c39a861acbd2a8048
Yu sebin <49060039+tpqls0327@users.noreply.github.com>
2022-08-25 23:18:51 +0900

    2022-08-25

---
+++ Baekjoon/DP/10942_팰린드롬_S.py
@@ -0,0 +1,24 @@
+# 10942 팰린드롬?
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    dp[i][i] = 1
+    
+for i in range(n-1):
+    if arr[i] == arr[i+1]:
+        dp[i][i+1] = 1
+       
+for i in range(2,n):    # 가운데부터 몇개 떨어졌나
+    for j in range(n-i):
+        if arr[j] == arr[j+i] and dp[j+1][i+j-1] == 1:
+            dp[j][j+i] = 1
+
+for _ in range(int(input())):
+    s,e = map(int, input().split())
+    print(dp[s-1][e-1])
+    

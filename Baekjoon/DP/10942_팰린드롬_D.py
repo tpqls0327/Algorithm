aa9238716396358235a4b44c6e39a68eac79830a
ponopono0322 <ponopono0322@gmail.com>
2022-08-25 23:15:57 +0900

    Create 10942_팰린드롬_D.py

---
+++ Baekjoon/DP/10942_팰린드롬_D.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = input().split()
+m = int(input())
+dp = [[0]*n for _ in range(n)]
+
+for i in range(n):
+    dp[i][i] = 1
+
+for i in range(n-1):
+    if arr[i] == arr[i+1]:
+        dp[i][i+1] = 1
+
+for i in range(2, n):
+    for j in range(n-i):
+        if arr[j] == arr[i+j] and dp[j+1][i+j-1] == 1:
+            dp[j][i+j] = 1
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(dp[s-1][e-1])

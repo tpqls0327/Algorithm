ca61f00639725738c974833d29e3db6453685266
Yu sebin <49060039+tpqls0327@users.noreply.github.com>
2022-08-26 12:24:33 +0900

    2022-08-26

--- Baekjoon/DP/10942_팰린드롬_S.py
+++ Baekjoon/DP/10942_팰린드롬_S.py
@@ -13,7 +13,7 @@ for i in range(n-1):
     if arr[i] == arr[i+1]:
         dp[i][i+1] = 1
        
-for i in range(2,n):    # 가운데부터 몇개 떨어졌나
+for i in range(2,n):    # 처음부터 몇개 떨어졌나 
     for j in range(n-i):
         if arr[j] == arr[j+i] and dp[j+1][i+j-1] == 1:
             dp[j][j+i] = 1

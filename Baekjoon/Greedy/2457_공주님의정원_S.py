947b77120ff672e8f4e3900bad4421ae31eb4736
Yu sebin <49060039+tpqls0327@users.noreply.github.com>
2022-08-30 23:25:32 +0900

    2022-08-30

---
+++ Baekjoon/Greedy/2457_공주님의정원_S.py
@@ -0,0 +1,23 @@
+# 2457 공주님의 정원
+
+n = int(input())
+flowers = []
+
+for _ in range(n):
+    sm, sd, em, ed = map(int, input().split())
+    flowers.append([sm*100+sd, em*100+ed])
+
+t = 301
+ans = 0
+while t < 1201:
+    nxt = t
+    for i in range(n):
+        if flowers[i][0] <= t and flowers[i][1] > nxt:
+            nxt = flowers[i][1]
+    if nxt == t:
+        print(0)
+        exit(0)
+    ans += 1
+    t = nxt
+
+print(ans)

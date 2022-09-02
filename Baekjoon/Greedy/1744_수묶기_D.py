8d102a0b6e6c376cf9243bbc92aa3e2996a7d9e0
ponopono0322 <ponopono0322@gmail.com>
2022-09-02 23:44:19 +0900

    Update 1744_수묶기_D.py

--- Baekjoon/Greedy/1744_수묶기_D.py
+++ Baekjoon/Greedy/1744_수묶기_D.py
@@ -17,19 +17,13 @@ else:
 
     t1, t2 = arr[:sep], arr[sep:]   # 배열 나누기
     n1, n2 = len(t1), len(t2)       # 두 배열 길이
-    if n1 <= 1:             # 길이가 1이하면
-        _sum += sum(t1)     # 더하기
-    else:                   # 길이가 2 이상
-        for i in range(0, n1-1, 2):
-            _sum += (t1[i] * t1[i + 1])
-        if n1 % 2:          # 홀수라면
-            _sum += t1[-1]  # 마지막 숫자 더해주기(가장 작은 것이 보장되므로)
-    if n2 <= 1:             # 마찬가지로 양수 배열에서도 진행
-        _sum += sum(t2)
-    else:
-        for i in range(n2-1, 0, -2):
-            _sum += (t2[i-1] * t2[i]) if t2[i-1] != 1 else (t2[i-1] + t2[i])
-        if n2 % 2:
-            _sum += t2[0]
+    for i in range(0, n1-1, 2):
+        _sum += (t1[i] * t1[i + 1])
+    if n1 % 2:
+        _sum += t1[-1]
+    for i in range(n2-1, 0, -2):
+        _sum += (t2[i-1] * t2[i]) if t2[i-1] != 1 else (t2[i-1] + t2[i])
+    if n2 % 2:
+        _sum += t2[0]
 
     print(_sum)

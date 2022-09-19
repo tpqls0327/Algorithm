6fde5716f6f03054c0a330aca941a9e566679647
ponopono0322 <ponopono0322@gmail.com>
2022-09-19 23:27:51 +0900

    Create 18869_멀티버스2_D.py

---
+++ Baekjoon/BinarySearch/18869_멀티버스2_D.py
@@ -0,0 +1,17 @@
+import sys
+from collections import defaultdict
+# input = sys.stdin.readline
+
+m, n = map(int, input().split())
+universe = defaultdict(int)
+for _ in range(m):
+    planets = list(map(int, input().split()))
+    keys = sorted(list(set(planets)))
+    ranks = {keys[i]: i for i in range(len(keys))}
+    add = tuple(ranks[x] for x in planets)
+    universe[add] += 1
+
+ans = 0
+for cnt in universe.values():
+    ans += cnt * (cnt - 1) // 2
+print(ans)
